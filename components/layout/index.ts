2ed0a42855dfa19a82ced541b556e2c716bbbd23
Casa Mineira SaaS <alexandrevendas.adm@gmail.com>
2026-06-26 13:39:25 -0300

    Enterprise foundation: organize SaaS structure and shared UI

---
+++ components/layout/index.ts
@@ -0,0 +1,4 @@
+export { AuthLayout } from "./AuthLayout";
+export { DashboardLayout } from "./DashboardLayout";
+export { PageShell } from "./PageShell";
+export { PublicLayout } from "./PublicLayout";
