09412a0353bdf80b0d0563893c3cbc57e1ebd8a1
Casa Mineira SaaS <alexandrevendas.adm@gmail.com>
2026-06-26 13:37:58 -0300

    Enterprise foundation: organize SaaS structure and shared UI

---
+++ components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Badge } from "./Badge";
+export { PremiumCard } from "./PremiumCard";
+export { PrimaryButton } from "./PrimaryButton";
+export { PremiumTextInput } from "./TextInput";
+export { SecondaryButton } from "./SecondaryButton";
+export { SectionHeader } from "./SectionHeader";
